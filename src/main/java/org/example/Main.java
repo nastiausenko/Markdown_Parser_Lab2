f8c9c84b25a009066c928139c645cca429e17390
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-28 22:04:10 +0200

    add flag --format logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,17 +11,42 @@ public class Main {
 
         String inputFile = args[0];
         String outputFile = null;
-        if (args.length > 2 && "--out".equals(args[1])) {
-            outputFile = args[2];
+        Format format = Format.HTML;
+
+        for (int i = 1; i < args.length; i++) {
+            if ("--out".equals(args[i])) {
+                if (i + 1 < args.length) {
+                    outputFile = args[i + 1];
+                } else {
+                    System.err.println("Missing value for --out flag");
+                    System.exit(1);
+                }
+            } else if ("--format".equals(args[i])) {
+                if (i + 1 < args.length) {
+                    try {
+                        format = Format.valueOf(args[i + 1].toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        System.err.println("Invalid value for --format flag [should be html/ansi]");
+                        System.exit(1);
+                    }
+                } else {
+                    System.err.println("Missing value for --format flag");
+                    System.exit(1);
+                }
+            }
         }
 
-        MarkdownParser markdownParser = new MarkdownParser();
+        processMarkdown(inputFile, outputFile, format);
+    }
+
+    private static void processMarkdown(String inputFile, String outputFile, Format format) {
         try {
-            FileManager fIleHandler = new FileManager(outputFile, inputFile);
-            String file = fIleHandler.readFile();
-            String markdown = markdownParser.parse(file);
+            FileManager fileHandler = new FileManager(outputFile, inputFile);
+            String file = fileHandler.readFile();
+            MarkdownParser markdownParser = new MarkdownParser(file);
+            String markdown = markdownParser.parse(format);
             if (outputFile != null) {
-                fIleHandler.writeFile(markdown);
+                fileHandler.writeFile(markdown);
             } else {
                 System.out.println(markdown);
             }
