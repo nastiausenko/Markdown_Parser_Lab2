824c7131993bf851b1cb34878360f061f0cac176
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-28 22:06:49 +0200

    fix tests for MarkdownParser

--- src/test/java/org/example/MarkdownParserTest.java
+++ src/test/java/org/example/MarkdownParserTest.java
@@ -1,18 +1,12 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class MarkdownParserTest {
     private MarkdownParser markdownParser;
-
-    @BeforeEach
-    public void beforeEach() {
-        markdownParser = new MarkdownParser();
-    }
 
     @ParameterizedTest
     @CsvSource({
@@ -21,38 +15,39 @@ class MarkdownParserTest {
             "`text`,<p><tt>text</tt></p>"
     })
     void markdownFormattingTest(String input, String expected) {
-        String result = markdownParser.parse(input);
-        Assertions.assertEquals(expected, result);
+        markdownParser = new MarkdownParser(input);
+        Assertions.assertEquals(expected, markdownParser.parse(Format.HTML));
     }
 
     @Test
     void preformattedMarkdownTest() {
-        String result = markdownParser.parse("""
+        String input = """
                 ```
                 _text_
-                ```""");
+                ```""";
         String expected = """
                 <p><pre>
                 _text_
                 </pre></p>""";
 
-        Assertions.assertEquals(expected,result);
+        markdownParser = new MarkdownParser(input);
+        Assertions.assertEquals(expected, markdownParser.parse(Format.HTML));
     }
 
     @Test
     void paragraphMarkdownTest() {
-        String result = markdownParser.parse("text" +
+        String input = "text" +
                 System.lineSeparator() +
                 "more text" +
                 System.lineSeparator() +
-                "and more text");
+                "and more text";
 
         String expected = "<p>text</p>" +
                 System.lineSeparator() +
                 "<p>more text</p>" +
                 System.lineSeparator() +
                 "<p>and more text</p>";
-
-        Assertions.assertEquals(expected, result);
+        markdownParser = new MarkdownParser(input);
+        Assertions.assertEquals(expected, markdownParser.parse(Format.HTML));
     }
 }
