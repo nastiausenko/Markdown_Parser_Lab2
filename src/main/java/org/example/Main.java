9184f6f8da2fc820b917870cbe73021c0d10101e
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-29 10:29:34 +0200

    refactor Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.err.println("java Main <inputFile> [--out outputFile]");
-            System.exit(1);
+            printUsageAndExit();
         }
 
         String inputFile = args[0];
@@ -15,28 +14,44 @@ public class Main {
 
         for (int i = 1; i < args.length; i++) {
             if ("--out".equals(args[i])) {
-                if (i + 1 < args.length) {
-                    outputFile = args[i + 1];
-                } else {
-                    System.err.println("Missing value for --out flag");
-                    System.exit(1);
-                }
+                outputFile = parseOutputFile(args, i);
             } else if ("--format".equals(args[i])) {
-                if (i + 1 < args.length) {
-                    try {
-                        format = Format.valueOf(args[i + 1].toUpperCase());
-                    } catch (IllegalArgumentException e) {
-                        System.err.println("Invalid value for --format flag [should be html/ansi]");
-                        System.exit(1);
-                    }
-                } else {
-                    System.err.println("Missing value for --format flag");
-                    System.exit(1);
-                }
+                format = parseFormat(args, i);
             }
         }
 
         processMarkdown(inputFile, outputFile, format);
+    }
+
+    private static void printUsageAndExit() {
+        System.err.println("java Main <inputFile> [--out outputFile] [--format html/ansi]");
+        System.exit(1);
+    }
+
+    private static String parseOutputFile(String[] args, int index) {
+        if (index + 1 < args.length) {
+            return args[index + 1];
+        } else {
+            System.err.println("Missing value for --out flag");
+            System.exit(1);
+            return null;
+        }
+    }
+
+    private static Format parseFormat(String[] args, int index) {
+        if (index + 1 < args.length) {
+            try {
+                return Format.valueOf(args[index + 1].toUpperCase());
+            } catch (IllegalArgumentException e) {
+                System.err.println("Invalid value for --format flag [should be html/ansi]");
+                System.exit(1);
+                return null;
+            }
+        } else {
+            System.err.println("Missing value for --format flag");
+            System.exit(1);
+            return null;
+        }
     }
 
     private static void processMarkdown(String inputFile, String outputFile, Format format) {
